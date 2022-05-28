c1b312e87edf4ab92289924f8527185dd09f1822
64x2 / Xofo <79061462+64x2@users.noreply.github.com>
2022-05-28 22:38:48 +0100

    Update gato-bot.py

--- gato-bot.py
+++ gato-bot.py
@@ -1,22 +1,39 @@
 import discord
 from discord.ext.commands import Bot
+from discord import Webhook
 from discord.ext import commands
-import asyncio
+import time
 
 # Put your discord bot's token here (You can get it from discord.com/developers/applications/)
 TOKEN = "Token Here"
 
 client = discord.Client()
-b = Bot(command_prefix = "!", self_bot=False)
+b = Bot = commands.Bot(
+command_prefix = '!',
+activity = discord.Activity(type=discord.ActivityType.watching, name="github.com/64x2/gato-bot")
+)
+
+
+@b.event
+async def on_ready():
+    channel = b.get_channel(974317715563089920) # This is for debug purposes, if you are going to use this, change the channel ID to your own :)
+    await channel.send("Gato-bot started up! :cat2:")
+    time.sleep(0.5)
+    await channel.send("Hope you day or night is going well! You can check my commands with `!help` and check the current version with `!verison`")
+    time.sleep(0.5)
+    await channel.send("Remember to star the repository if you enjoy (https://github.com/64x2/gato-bot)")
+    time.sleep(0.5)
+    print("Gato-bot.py has started up on the frontend! Enjoy the bot :^)")
 
 @b.command()
-async def message(ctx, user:discord.Member, *, message=None):
-    await user.send("This is a test command, if you recieved this message, then hi :cat2:")
-    print("Message command was used on "+user.name+"#"+user.discriminator)
+async def debug(ctx, arg):
+    await ctx.send(arg)
+    print("The command 'Debug' was just used for testing purposes!")
+
 
 @b.command()
 async def version(ctx):
-    embed=discord.Embed(title="Gato-bot.py Alpha 2a", url="https://cdn.discordapp.com/avatars/471386638984151042/fca27b6741fb03c80a0a34034a9ba44f.png", description="Gato-bot.py is property of Xofo LLC. Unauthorized use and/or abuse of Xofo LLC property isn't nice :(", color=800080)
+    embed=discord.Embed(title="Gato-bot.py Beta 2", url="https://soundcloud.com/aprilboyy", description="Gato-bot.py is property of Xofo LLC. Unauthorized use and/or abuse of Xofo LLC property isn't nice :(", color=800080)
     await ctx.send(embed=embed)
     print("Command Version was used")
 
