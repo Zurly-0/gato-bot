3c1afc052e90a2e0c3babf6e37a871dbe3618143
64x2 / Xofo <79061462+64x2@users.noreply.github.com>
2022-05-30 09:43:02 +0100

    Fix invalid links

--- gato-bot.py
+++ gato-bot.py
@@ -11,16 +11,12 @@ TOKEN = os.getenv("TOKEN")
 client = discord.Client()
 b = Bot = commands.Bot(
 command_prefix = '!',
-activity = discord.Activity(type=discord.ActivityType.watching, name="github.com/64x2/gato-bot")
+activity = discord.Activity(type=discord.ActivityType.watching, name="github.com/64x2/gato-bot-public")
 )
 
 
 @b.event
 async def on_ready():
-    channel = b.get_channel(974317715563089920) # This is for debug purposes, if you are going to use this, change the channel ID to your own :)
-    await channel.send("GatoBot has started up, this text will be removed in stable releases")
-    time.sleep(1)
-    await channel.send("You're running version `Beta 5`! This version is a beta, expect bugs :cat2:")
     print("  ________        __        __________        __    ")
     print(" /  _____/_____ _/  |_  ____\______   \ _____/  |_  ")
     print("/   \  ___\__  \\   __\/  _ \|    |  _//  _ \   __\ ")
@@ -28,7 +24,7 @@ async def on_ready():
     print(" \______  (____  /__|  \____/|______  /\____/|__|   ")
     print("        \/     \/                   \/              ")
     print("")
-    print("GatoBot Beta 5 by Xofo. Avaliable at github.com/64x2/gato-bot")
+    print("GatoBot Public by Xofo. Avaliable at github.com/64x2/gato-bot-public")
 
 @b.command()
 async def debug(ctx, arg):
@@ -45,7 +41,7 @@ async def ping(ctx):
 
 @b.command()
 async def version(ctx):
-    embed=discord.Embed(title="Gato-bot.py Beta 5", url="https://github.com/64x2/gato-bot", description="Gato-bot.py is property of Xofo LLC. Unauthorized use and/or abuse of Xofo LLC property isn't nice :(", color=800080)
+    embed=discord.Embed(title="Gato-bot.py Beta 5", url="https://github.com/64x2/gato-bot-public", description="Gato-bot.py is property of Xofo LLC. Unauthorized use and/or abuse of Xofo LLC property isn't nice :(", color=800080)
     await ctx.send(embed=embed)
     print("Command Version was used")
 
